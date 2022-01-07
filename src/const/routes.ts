af83e61fa302cd978cec8728ca1be366d2024a78
guohaomiao <1662673658@qq.com>
2022-01-08 01:43:11 +0800

    bug: ts类型修复

--- src/const/routes.ts
+++ src/const/routes.ts
@@ -2,25 +2,25 @@ import { routesInterface } from '@/types/routes'
 
 export const routes: routesInterface[] = [
   {
-    label: '仪表盘',
+    title: '仪表盘',
     value: 'board',
     path: '/',
     file: 'main/board/index'
   },
   {
-    label: '常规',
+    title: '常规',
     value: 'normal',
     path: '/normal',
     file: 'main/normal/index',
     children: [
       {
-        label: '用户列表',
+        title: '用户列表',
         value: 'user',
         path: '/normal/user',
         file: 'main/normal/user'
       },
       {
-        label: '菜单管理',
+        title: '菜单管理',
         value: 'menu',
         path: '/normal/menu',
         file: 'main/normal/menu'
@@ -28,13 +28,13 @@ export const routes: routesInterface[] = [
     ]
   },
   {
-    label: 'three',
+    title: 'three',
     value: 'three',
     path: '/three',
     file: 'main/three/index',
     children: [
       {
-        label: '地球',
+        title: '地球',
         value: 'earthThree',
         path: '/three/earth',
         file: 'main/three/earth'
@@ -42,13 +42,13 @@ export const routes: routesInterface[] = [
     ]
   },
   {
-    label: 'cesium',
+    title: 'cesium',
     value: 'cesium',
     path: '/cesium',
     file: 'main/cesium/index',
     children: [
       {
-        label: '地球',
+        title: '地球',
         value: 'earthCesium',
         path: '/cesium/earth',
         file: 'main/cesium/earth'
